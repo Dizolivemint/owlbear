c9dd3b206b11340689b0675b026d5a43a8c69cc8
Miles Exner <somabhi@gmail.com>
2023-05-18 09:03:17 -0700

    🔍 add open graph

--- components/head/index.tsx
+++ components/head/index.tsx
@@ -9,6 +9,15 @@ const HeadContent: React.FC<HeadContentProps> = (props) => {
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:title" content="AI Monster Generator" />
+        <meta
+          property="og:description"
+          content="DnD 5e Monster Generator and Battle Map"
+        />
+        <meta
+          property="og:image"
+          content="https://battle-map.vercel.com/owlbear.jpg"
+        />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
