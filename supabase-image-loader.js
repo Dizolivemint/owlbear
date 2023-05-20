309a90c55665bac596dedba5aa8385e3a8828424
Miles Exner <somabhi@gmail.com>
2023-05-19 19:04:46 -0700

    🧑‍💻 more image src options

--- supabase-image-loader.js
+++ supabase-image-loader.js
@@ -1,5 +1,12 @@
-const projectId = 'jhvfbckjvpmoahabhfth'
-
 export default function supabaseLoader({ src, width, quality }) {
-  return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&resize=contain&quality=${quality || 75}`
+  const projectId = 'jhvfbckjvpmoahabhfth';
+
+  if (src.startsWith('/public/')) {
+    // Local image from the public directory
+    const publicPath = src.replace('/public', '');
+    return publicPath;
+  } else {
+    // Supabase image
+    return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&resize=contain&quality=${quality || 75}`;
+  }
 }
