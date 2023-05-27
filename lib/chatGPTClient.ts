51a1e8b8fbc68774456c97283eb9edc98c149912
Miles Exner <somabhi@gmail.com>
2023-05-26 21:55:13 -0700

    ✨ update to stable diffusion image generation

--- lib/chatGPTClient.ts
+++ lib/chatGPTClient.ts
@@ -13,12 +13,14 @@ export class ChatGPTClient {
   private apiEndpoint: string;
   private apiImageEndpoint: string;
   private bucket: string;
+  private stablediffusionApiKey: string;
 
   constructor() {
     this.apiKey = process.env.CHATGPT_API_KEY || '';
     this.apiEndpoint = 'https://api.openai.com/v1/completions';
-    this.apiImageEndpoint = 'https://api.openai.com/v1/images/generations';
+    this.apiImageEndpoint = process.env.STABLEDIFFUSION_API_ENDPOINT || '';
     this.bucket = 'images_public';
+    this.stablediffusionApiKey = process.env.STABLEDIFFUSION_API_KEY || '';
   }
 
   private checkKey() {
@@ -27,17 +29,7 @@ export class ChatGPTClient {
     }
   }
 
-  async generateCharacter(request: string): Promise<GenerateCharacterResponse> {
-    this.checkKey();
-  
-    const { size, species, challengeRating, isLegendary } = JSON.parse(request);
-
-    const prompt = isLegendary ?
-    `Create a Dungeons and Dragons 5e ${size} ${species} with the challenge rating of ${challengeRating}. Present the data in the following JSON string format in one line (i.e., no line breaks): { "name": string, "background": string, "appearance": string, "attributes": { "STR": number, "DEX": number, "CON": number, "INT": number, "WIS": number, "CHA": number }, "skills": [{ "skill": string, "description": string }], "actions": [{ "action": string, "description": string }], "reactions": [{ "reaction": string, "description": string }], "legendary_actions": [{ "legendary_action": string, "description": string }] }. When applicable, skill, action, and reaction descriptions should include the dice modifier (e.g., +5) or dice roll (e.g., 2d8) and the damage type (e.g., slashing, fire).`
-    :
-    `Create a Dungeons and Dragons 5e ${size} ${species} with the challenge rating of ${challengeRating}. Present the data in the following JSON string format in one line (i.e., no line breaks): { "name": string, "background": string, "appearance": string, "attributes": { "STR": number, "DEX": number, "CON": number, "INT": number, "WIS": number, "CHA": number }, "skills": [{ "skill": string, "description": string }], "actions": [{ "action": string, "description": string }], "reactions": [{ "reaction": string, "description": string }] }. When applicable, skill, action, and reaction descriptions should include the dice modifier (e.g., +5) or dice roll (e.g., 2d8) and the damage type (e.g., slashing, fire).`
-  
-    console.log('prompt', prompt)
+  async requestGPT(prompt: string): Promise<any> {
     const requestBody = {
       "model": "text-davinci-003",
       "prompt": prompt,
@@ -64,105 +56,176 @@ export class ChatGPTClient {
       const uint8ArrayResponseBody = await response.arrayBuffer(); // Get response body as Uint8Array
       const responseBody = JSON.parse(new TextDecoder().decode(uint8ArrayResponseBody)); // Parse the JSON response
 
-      console.log('responseBody', responseBody)
-
-      const character: Character = {
-        name: '',
-        species: species,
-        challenge_rating: challengeRating,
-        attributes: {
-          STR: 0,
-          DEX: 0,
-          CON: 0,
-          INT: 0,
-          WIS: 0,
-          CHA: 0,
-        },
-        skills: [{ skill: '', description: '' }],
-        actions: [{ action: '', description: '' }],
-        reactions: [{ reaction: '', description: ''}],
-        description: '',
-        background: '',
-        size,
-        appearance: '',
-      };
-
-      // Get the first choice text, trim it, sanitize it, and parse it as JSON
-      const choiceText = responseBody.choices[0].text.trim();
-      const jsonStartIndex = choiceText.indexOf('{');
-      const jsonEndIndex = choiceText.lastIndexOf('}') + 1;
-      const trimmedJson = choiceText.slice(jsonStartIndex, jsonEndIndex);
-      const parsedData = JSON.parse(trimmedJson);
-
-      // Assign the parsed values to the character object
-      character.attributes = getPropInsensitive(parsedData, 'attributes');
-      if (!character.attributes || Object.values(character.attributes).some(val => !val)) {
-        throw new Error('Attributes are empty or undefined');
-      }
-
-      character.skills = getPropInsensitive(parsedData, 'skills');
-      if (!character.skills || Object.values(character.skills).some(val => !val)) {
-        throw new Error('Skills are empty or undefined');
-      }
-
-      character.actions = getPropInsensitive(parsedData, 'actions');
-      if (!character.actions || Object.values(character.actions).some(val => !val)) {
-        throw new Error('Actions are empty or undefined');
-      }
-
-      character.reactions = getPropInsensitive(parsedData, 'reactions');
-
-      if (isLegendary) character.legendary_actions = getPropInsensitive(parsedData, 'legendary_actions');
-
-      character.background = getPropInsensitive(parsedData, 'background');
-      if (!character.background) {
-        throw new Error('Background is empty or undefined');
-      }
-
-      character.name = getPropInsensitive(parsedData, 'name');
-      if (!character.name) {
-        throw new Error('Name is empty or undefined');
-      }
-
-      
-      character.appearance = getPropInsensitive(parsedData, 'appearance');
-      if (!character.appearance) {
-        throw new Error('Appearance is empty or undefined');
-      }
-
-      // Get image url
-      const images = await this.createImage(character.name, character.appearance);
-      if (!images) {
-        throw new Error('Image url is empty or undefined');
-      }
-      const imageUrl = images[0];
-      console.log('imageUrl', imageUrl);
-
-      return { character, imageUrl };
+      return responseBody;
     } catch (error) {
-      console.error('Error generating character:', error);
-      return { error: 'Error generating character' };
+      console.error('ChatGPT', error);
+      return { error: 'Error from ChatGPT' };
     }
   }
 
-  async createImage(name: string, description: string) {
-    type ApiResponse = {
-      created: number;
-      data: { url: string }[];
-    }    
+  async generateCharacter(request: string): Promise<GenerateCharacterResponse> {
+    this.checkKey();
+  
+    const { size, species, challengeRating, isLegendary } = JSON.parse(request);
 
+    const prompt = isLegendary ?
+    `Create a Dungeons and Dragons 5e ${size} ${species} with the challenge rating of ${challengeRating}. Present the data in the following JSON string format in one line (i.e., no line breaks): { "name": string, "background": string, "appearance": string, "attributes": { "STR": number, "DEX": number, "CON": number, "INT": number, "WIS": number, "CHA": number }, "skills": [{ "skill": string, "description": string }], "actions": [{ "action": string, "description": string }], "reactions": [{ "reaction": string, "description": string }], "legendary_actions": [{ "legendary_action": string, "description": string }] }. When applicable, skill, action, and reaction descriptions should include the dice modifier (e.g., +5) or dice roll (e.g., 2d8) and the damage type (e.g., slashing, fire).`
+    :
+    `Create a Dungeons and Dragons 5e ${size} ${species} with the challenge rating of ${challengeRating}. Present the data in the following JSON string format in one line (i.e., no line breaks): { "name": string, "background": string, "appearance": string, "attributes": { "STR": number, "DEX": number, "CON": number, "INT": number, "WIS": number, "CHA": number }, "skills": [{ "skill": string, "description": string }], "actions": [{ "action": string, "description": string }], "reactions": [{ "reaction": string, "description": string }] }. When applicable, skill, action, and reaction descriptions should include the dice modifier (e.g., +5) or dice roll (e.g., 2d8) and the damage type (e.g., slashing, fire).`
+  
+    console.log('prompt', prompt)
+    
+    
+    const responseBody = await this.requestGPT(prompt);
+    if (responseBody.error) {
+      throw new Error(responseBody.error);
+    }
+
+    console.log('responseBody', responseBody)
+
+    const character: Character = {
+      name: '',
+      species: species,
+      challenge_rating: challengeRating,
+      attributes: {
+        STR: 0,
+        DEX: 0,
+        CON: 0,
+        INT: 0,
+        WIS: 0,
+        CHA: 0,
+      },
+      skills: [{ skill: '', description: '' }],
+      actions: [{ action: '', description: '' }],
+      reactions: [{ reaction: '', description: ''}],
+      description: '',
+      background: '',
+      size,
+      appearance: '',
+    };
+
+    // Get the first choice text, trim it, sanitize it, and parse it as JSON
+    const choiceText = responseBody.choices[0].text.trim();
+    const jsonStartIndex = choiceText.indexOf('{');
+    const jsonEndIndex = choiceText.lastIndexOf('}') + 1;
+    const trimmedJson = choiceText.slice(jsonStartIndex, jsonEndIndex);
+    const parsedData = JSON.parse(trimmedJson);
+
+    // Assign the parsed values to the character object
+    character.attributes = getPropInsensitive(parsedData, 'attributes');
+    if (!character.attributes || Object.values(character.attributes).some(val => !val)) {
+      throw new Error('Attributes are empty or undefined');
+    }
+
+    character.skills = getPropInsensitive(parsedData, 'skills');
+    if (!character.skills || Object.values(character.skills).some(val => !val)) {
+      throw new Error('Skills are empty or undefined');
+    }
+
+    character.actions = getPropInsensitive(parsedData, 'actions');
+    if (!character.actions || Object.values(character.actions).some(val => !val)) {
+      throw new Error('Actions are empty or undefined');
+    }
+
+    character.reactions = getPropInsensitive(parsedData, 'reactions');
+
+    if (isLegendary) character.legendary_actions = getPropInsensitive(parsedData, 'legendary_actions');
+
+    character.background = getPropInsensitive(parsedData, 'background');
+    if (!character.background) {
+      throw new Error('Background is empty or undefined');
+    }
+
+    character.name = getPropInsensitive(parsedData, 'name');
+    if (!character.name) {
+      throw new Error('Name is empty or undefined');
+    }
+    
+    character.appearance = getPropInsensitive(parsedData, 'appearance');
+    if (!character.appearance) {
+      throw new Error('Appearance is empty or undefined');
+    }
+
+    // Create a Stable Diffusion prompt using the character's name and appearance
+    const chatGptStableDiffusionPrompt = `turn the appearance at the end into boorus tags separated by commas in a one line prompt. include these tags in the beginning of the prompt 'High detail RAW color (Digital painting:1.2), of ${character.name}'. at the end of the prompt, add the tags ', best quality, trending on artstation, unreal engine' appearance: ${character.appearance}`
+
+    // Get the Stable Diffusion prompt
+    const stableDiffusionPromptResponse = await this.requestGPT(chatGptStableDiffusionPrompt);
+    if (stableDiffusionPromptResponse.error) {
+      throw new Error(stableDiffusionPromptResponse.error);
+    }
+    const stableDiffusionPrompt = stableDiffusionPromptResponse.choices[0].text.trim();
+
+    // Get image url
+    const images = await this.createImage(character.name, stableDiffusionPrompt);
+    if (!images) {
+      throw new Error('Image url is empty or undefined');
+    }
+    const imageUrl = images[0];
+    console.log('imageUrl', imageUrl);
+
+    return { character, imageUrl };
+  }
+
+  async createImage(name: string, prompt: string): Promise<string[] | string> {
+    type ApiResponse = {
+      status: string;
+      generationTime: number;
+      id: number;
+      output: string[];
+      meta: {
+        prompt: string;
+        model_id: string;
+        negative_prompt: string;
+        scheduler: string;
+        safety_checker: string;
+        W: number;
+        H: number;
+        guidance_scale: number;
+        seed: number;
+        steps: number;
+        n_samples: number;
+        full_url: string;
+        upscale: string;
+        multi_lingual: string;
+        panorama: string;
+        self_attention: string;
+        embeddings: null;
+        lora: null;
+        outdir: string;
+        file_prefix: string;
+      };
+    };
+     
     const requestBody = {
-      "prompt": description,
-      "n": 1,
-      "size": "512x512"
+      "key": this.stablediffusionApiKey,
+      "model_id": "deliberate-v2",
+      prompt,
+      negative_prompt: "((cartoon)), (painting), (doll), ((drawing)), ((out of focus body)), ((out of focus face)), ((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), [out of frame], (extra fingers), (mutated hands), ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck))), ((cross-eyed)), cross eyed, ((big ears)), (text), (((watermark))), (watermarking)",
+      "width": "512",
+      "height": "512",
+      "samples": "1",
+      "num_inference_steps": "30",
+      "safety_checker": "no",
+      "enhance_prompt": "no",
+      "seed": null,
+      "guidance_scale": 7.5,
+      "multi_lingual": "no",
+      "panorama": "no",
+      "self_attention": "no",
+      "upscale": "no",
+      "embeddings_model": "",
+      "lora_model": "",
+      "scheduler": "DDPMScheduler",
+      "webhook": null,
+      "track_id": null
     };
 
     try {
       const response = await fetch(this.apiImageEndpoint, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this.apiKey}`,
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(requestBody),
       });
@@ -177,14 +240,14 @@ export class ChatGPTClient {
       console.log('responseBody', responseBody)
 
       // Upload images to Supabase storage
-      const imageUrls = responseBody.data;
+      const imageUrls = responseBody.output;
       if (!imageUrls || imageUrls.length === 0) {
         throw new Error('No images generated');
       }
       const uploadedImages = await Promise.all(
         imageUrls.map(async (url, index) => {
           // Fetch the image as a Buffer
-          const imageResponse = await fetch(url.url);
+          const imageResponse = await fetch(url);
           const imageArrayBuffer = await imageResponse.arrayBuffer();
           const imageBuffer = Buffer.from(imageArrayBuffer);
 
@@ -211,6 +274,7 @@ export class ChatGPTClient {
       return uploadedImages.filter((url) => url !== null) as string[];
     } catch (error) {
       console.error('Error generating image:', error);
+      return '';
     }
   }
 }
