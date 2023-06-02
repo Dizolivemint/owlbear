b7ad9a68892922fa6d03b0bdfb44564815b1404d
Miles Exner <somabhi@gmail.com>
2023-06-02 10:41:35 -0700

    🐛 fix redirectTo

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -21,9 +21,9 @@ export default function Login() {
 
   const getURL = () => {
     let url =
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      process?.env?.NEXT_PUBLIC_URL ?? // Automatically set by Vercel.
       'http://localhost:3000/';
-    url = url.includes('http') ? url : `https://${url}`;
+    url = url.includes('http') ? `https://${url}` : url;
     // Make sure to including trailing `/`.
     url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
     return url;
